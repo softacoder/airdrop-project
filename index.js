6a5b28df6673a053bedbb97544558319ee065ef5
softacoder <113185323+softacoder@users.noreply.github.com>
2024-11-13 13:55:28 +0000

    commit

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+// const {
+//   Connection,
+//   PublicKey,
+//   clusterApiUrl,
+//   Keypair,
+//   LAMPORTS_PER_SOL,
+// } = require("@solana/web3.js");
+
+// const wallet = new Keypair();
+
+// const publicKey = new PublicKey(wallet._keypair.publicKey);
+// const secretKey = wallet._keypair.secretKey;
+
+// const getWalletBalance = async () => {
+//   try {
+//     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+//     const walletBalance = await connection.getBalance(publicKey);
+//     console.log(`Wallet balance is ${walletBalance}`);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// const airDropSol = async () => {
+//   try {
+//     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+//     const fromAirDropSignature = await connection.requestAirdrop(
+//       publicKey,
+//       2 * LAMPORTS_PER_SOL
+//     );
+//     await connection.confirmTransaction(fromAirDropSignature);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// const main = async () => {
+//   await getWalletBalance();
+//   await airDropSol();
+//   await getWalletBalance();
+// };
+// main();
+
 const {
   Connection,
   PublicKey,
@@ -27,7 +68,12 @@ const airDropSol = async () => {
       publicKey,
       2 * LAMPORTS_PER_SOL
     );
-    await connection.confirmTransaction(fromAirDropSignature);
+    const latestBlockHash = await connection.getLatestBlockhash();
+    await connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: fromAirDropSignature,
+    });
   } catch (err) {
     console.log(err);
   }
@@ -35,5 +81,6 @@ const airDropSol = async () => {
 const main = async () => {
   await getWalletBalance();
   await airDropSol();
+  await getWalletBalance();
 };
 main();
